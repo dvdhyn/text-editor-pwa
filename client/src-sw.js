7268ca4d219fe7a20683371d16e88665786051b7
yungtaco <dvhyun@gmail.com>
2024-03-20 04:24:16 -0400

    Applied asset caching to service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'image', 'worker'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60,
+        maxEntries: 50,
+      }),
+    ],
+  })
+);
